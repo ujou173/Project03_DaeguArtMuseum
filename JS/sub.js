f51ba5559158e5ae5461d57ca4a309f7383e42a7
ujou173 <ujou173@gmail.com>
2022-12-01 14:23:54 +0900

    22.12.01 제출

--- JS/sub.js
+++ JS/sub.js
@@ -46,3 +46,22 @@ window.addEventListener("resize", function () {
     }, 200);
   }
 });
+
+// 상세검색 버튼
+const $searchBtn = document.querySelector(".more_search");
+const $searchBg = document.querySelector(".search_bg");
+const $searchModal = document.querySelector(".search_modal");
+
+$searchBtn.addEventListener("click", function () {
+  // console.log("click");
+  $searchBg.classList.add("on");
+  $searchModal.classList.add("on");
+});
+
+// 상세검색 닫기 버튼
+const $modalClose = document.querySelector(".modal_title > button");
+
+$modalClose.addEventListener("click", function () {
+  $searchBg.classList.remove("on");
+  $searchModal.classList.remove("on");
+});
