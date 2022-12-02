0605a4d4e4aba4c7362d6bd5b726f8d266bc3bb5
ujou173 <ujou173@gmail.com>
2022-12-02 16:07:20 +0900

    index와 sub에 공통으로 들어가는 스크립트

--- JS/common.js
+++ JS/common.js
@@ -1,3 +1,83 @@
+// gnb 변수 선언
+const gnb = `
+<ul>
+<li>
+  <a href="#">전시</a>
+  <ul class="sub">
+    <li><a href="#">현재전시</a></li>
+    <li><a href="#">지난전시</a></li>
+    <li><a href="#">예정전시</a></li>
+  </ul>
+</li>
+<li>
+  <a href="#">교육 및 행사</a>
+  <ul class="sub">
+    <li><a href="#">렉쳐</a></li>
+    <li><a href="#">전시연계</a></li>
+    <li><a href="#">이벤트</a></li>
+  </ul>
+</li>
+<li>
+  <a href="./sub.html">소장품</a>
+  <ul class="sub">
+    <li><a href="./sub.html">소장품 검색</a></li>
+    <li><a href="#">이건희 컬렉션</a></li>
+  </ul>
+</li>
+<li>
+  <a href="#">디지털 미술관</a>
+  <ul class="sub">
+    <li><a href="#">디지털 미술관</a></li>
+    <li><a href="#">미술 정보 센터</a></li>
+    <li><a href="#">아카이브실</a></li>
+    <li><a href="#">디지털 가상공간</a></li>
+    <li><a href="#">사진자료</a></li>
+    <li><a href="#">발간자료</a></li>
+  </ul>
+</li>
+<li>
+  <a href="#">미술상</a>
+  <ul class="sub">
+    <li><a href="#">이인성미술상</a></li>
+    <li><a href="#">정점식미술상</a></li>
+  </ul>
+</li>
+<li>
+  <a href="#">커뮤니티</a>
+  <ul class="sub">
+    <li><a href="#">게시판</a></li>
+    <li><a href="#">관람후기</a></li>
+    <li><a href="#">네트워크</a></li>
+  </ul>
+</li>
+<li>
+  <a href="#">공지사항</a>
+  <ul class="sub">
+    <li><a href="#">공지사항</a></li>
+    <li><a href="#">새 소식</a></li>
+    <li><a href="#">메일링 서비스</a></li>
+    <li><a href="#">보도자료</a></li>
+  </ul>
+</li>
+<li>
+  <a href="#">미술관 소개</a>
+  <ul class="sub">
+    <li><a href="#">대구미술관</a></li>
+    <li><a href="#">관람안내</a></li>
+    <li><a href="#">오시는길</a></li>
+    <li><a href="#">서포터즈</a></li>
+    <li><a href="#">구성원</a></li>
+    <li><a href="#">시설안내</a></li>
+  </ul>
+</li>
+</ul>`;
+
+// gnb 출력
+const $hdGnb = document.querySelector(".hd_bottom > nav");
+$hdGnb.innerHTML = gnb;
+const $hamGnb1 = document.querySelector(".hd_ham > nav");
+$hamGnb1.innerHTML = gnb;
+
 // .lang 하위의 버튼
 const $lang = document.querySelector(".lang button");
 const $langList = document.querySelector(".lang ul");
@@ -44,259 +124,5 @@ window.addEventListener("resize", function () {
       $hamGnb.classList.remove("on");
       $hamLang.classList.remove("on");
     }, 200);
-  }
-});
-
-// sec1 메인배너 슬라이드
-var mainBannerSl = new Swiper(".main_banner", {
-  loop: true,
-  autoplay: {
-    delay: 5500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".main_banner_pg",
-    clickable: true,
-  },
-});
-
-// sec2 tab menu 버튼
-const $tabBtn = document.querySelectorAll(".sec2 .tab_btns > button");
-const $cardCon = document.querySelectorAll(".sec2 .card_con > div");
-
-$tabBtn.forEach(function (item, idx) {
-  item.addEventListener("click", function () {
-    btnOn(idx);
-    cardOn(idx);
-  });
-});
-
-function btnOn(num) {
-  $tabBtn.forEach(function (Tbtn, i) {
-    Tbtn.classList.remove("on");
-  });
-  $tabBtn[num].classList.add("on");
-}
-
-function cardOn(num1) {
-  $cardCon.forEach(function (Tcard, i) {
-    Tcard.classList.remove("on");
-  });
-  $cardCon[num1].classList.add("on");
-}
-
-// sec2 새소식 슬라이드
-var newsSl = new Swiper(".news_list", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  breakpoints: {
-    500: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    700: {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-    1150: {
-      slidesPerView: 3,
-      spaceBetween: 50,
-    },
-    1500: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
-  navigation: {
-    nextEl: ".news_next",
-    prevEl: ".news_prev",
-  },
-});
-
-// sec2 공지사항 슬라이드
-var noticeSl = new Swiper(".notice_list", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  breakpoints: {
-    500: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    700: {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-    1150: {
-      slidesPerView: 3,
-      spaceBetween: 50,
-    },
-    1500: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
-  navigation: {
-    nextEl: ".notice_next",
-    prevEl: ".notice_prev",
-  },
-});
-
-// sec2 보도자료 슬라이드
-var mediaSl = new Swiper(".media_list", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  breakpoints: {
-    500: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    700: {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-    1150: {
-      slidesPerView: 3,
-      spaceBetween: 50,
-    },
-    1500: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
-  navigation: {
-    nextEl: ".media_next",
-    prevEl: ".media_prev",
-  },
-});
-
-//sec4 디지털 미술관 동영상 슬라이드
-
-var youtubeSl = new Swiper(".digital_museum_slide", {
-  initialSlide: 1,
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 1,
-  breakpoints: {
-    700: {
-      slidesPerView: "auto",
-    },
-  },
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 200,
-    depth: 100,
-    modifier: 1.5,
-    slideShadows: false,
-  },
-  navigation: {
-    nextEl: ".digital_museum_next",
-    prevEl: ".digital_museum_prev",
-  },
-});
-
-//sec4 디지털 미술관 동영상 클릭시 play
-// iframe 사용으로 일단 보류
-// iframe 제거
-const $vidList = document.querySelectorAll(".digital_museum_slide li"),
-  $btnNext = document.querySelector(".digital_museum_next"),
-  $btnPrev = document.querySelector(".digital_museum_prev"),
-  $stop = document.querySelectorAll(".digital_museum_slide li video");
-let vidToggle = 0;
-// console.log($vidList);
-// console.log($stop);
-
-$vidList.forEach(function (item, idx) {
-  item.addEventListener("click", function () {
-    let vidPlay = this.childNodes[3];
-    if (vidToggle == 0) {
-      vidPlay.play();
-      vidToggle = 1;
-      // console.log(vidPlay);
-      // console.log(vidToggle);
-    } else {
-      if (vidToggle == 1) {
-        vidPlay.pause();
-        vidToggle = 0;
-        // console.log(vidPlay);
-        // console.log(vidToggle);
-      }
-    }
-  });
-});
-
-$btnNext.addEventListener("click", function () {
-  $stop.forEach(function (item, idx) {
-    item.pause();
-    vidToggle = 0;
-  });
-});
-
-$btnPrev.addEventListener("click", function () {
-  $stop.forEach(function (item, idx) {
-    item.pause();
-    vidToggle = 0;
-  });
-});
-
-// 스와이프시 중앙의 포커스 된 동영상을 제외하고 나머지 동영상 자동 정지
-window.addEventListener("mouseup", function () {
-  if ($vidList[1].classList.contains("swiper-slide-next")) {
-    $vidList[1].querySelector("video").pause();
-    $vidList[2].querySelector("video").pause();
-  } else if ($vidList[1].classList.contains("swiper-slide-active")) {
-    $vidList[0].querySelector("video").pause();
-    $vidList[2].querySelector("video").pause();
-  } else if ($vidList[1].classList.contains("swiper-slide-prev")) {
-    $vidList[1].querySelector("video").pause();
-    $vidList[2].querySelector("video").pause();
-  }
-});
-
-// sec5 mobile 자동 슬라이드
-const $colWrap = document.querySelector(".sec5 > div");
-const $colCon = document.querySelector(".collection");
-const $colList = document.querySelectorAll(".collection_list");
-
-let slideCount = $colList.length;
-let firstList = $colCon.firstElementChild;
-let lastList = $colCon.lastElementChild;
-let slideEvent = 0;
-
-// 자동 슬라이드 함수
-function colSL() {
-  $colCon.style.cssText = `margin-left:${-bodyWd}px; transition:0.5s`;
-  console.log(bodyWd);
-  setTimeout(() => {
-    $colCon.insertBefore(firstList, null);
-    $colCon.style = `margin-left: 0`;
-  }, 500);
-  firstList = $colCon.firstElementChild;
-  lastList = $colCon.lastElementChild;
-}
-
-// pc의 sec5 animation 효과 제거 후 자동 슬라이드 함수 실행
-window.addEventListener("resize", function () {
-  // bodyWd = document.body.offsetWidth + 17;
-  bodyWd = document.body.offsetWidth;
-  // console.log(bodyWd);
-  if (bodyWd + 17 < 701) {
-    $colWrap.classList.remove("on");
-    $colWrap.classList.add("off");
-    if (slideEvent == 0) {
-      slideEvent++;
-      slideInterval = setInterval(colSL, 5000);
-      console.log("자동 슬라이드 실행");
-    } else {
-      // console.log("실행 안함");
-    }
-  } else if (bodyWd + 17 > 700) {
-    $colWrap.classList.remove("off");
-    $colWrap.classList.add("on");
-    if (slideEvent >= 1) {
-      clearInterval(slideInterval);
-      slideEvent = 0;
-      console.log("자동 슬라이드 종료");
-    }
   }
 });
